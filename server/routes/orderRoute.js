81d1a0f1f8e71e3ee07984173229b3bdf4810c52
pawan bhandari <bhandaripawan769@gmail.com>
2025-04-11 16:55:14 +0545

    order route changed for usgetUserOrder

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -1,26 +1,30 @@
 // const express = require('express');
-import express from 'express';
-import { createOrder, getAllOrder, getOrder, getUserOrder, updateOrderStatus, orderSummary } from '../controllers/orderController.js';
-import {protect, restrictTo} from '../controllers/authController.js'
+import express from "express";
+import {
+  createOrder,
+  getAllOrder,
+  getOrder,
+  getUserOrder,
+  updateOrderStatus,
+  orderSummary,
+} from "../controllers/orderController.js";
+import { protect, restrictTo } from "../controllers/authController.js";
 
 const router = express.Router();
 
+router.route("/summary").get(protect, restrictTo("admin"), orderSummary);
 
-router.route("/summary")
-  .get(protect, restrictTo("admin"), orderSummary);
-
-router.route("/")
+router
+  .route("/")
   .post(protect, createOrder)
   .get(protect, restrictTo("admin"), getAllOrder);
 
-router.route("/user/:id")
-  .get(protect, restrictTo("admin"), getUserOrder);
+router.route("/user/:id").get(protect, getUserOrder);
 
-router.route("/:id")
-  .get(getOrder)
+router.route("/:id").get(protect, restrictTo("admin"), getOrder);
 
-router.route("/status/:id")
-  .patch(protect, updateOrderStatus);
-
+router
+  .route("/status/:id")
+  .patch(protect, restrictTo("admin"), updateOrderStatus);
 
 export default router;
